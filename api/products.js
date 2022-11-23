60a6cea55d6d5c1d02896b22069dfef6f8a3b9fa
GarrHFullstack <Garrett.hall@ttu.edu>
2022-11-23 16:22:09 -0600

    added api and index

--- api/products.js
+++ api/products.js
@@ -1,17 +1,52 @@
 const express = require("express")
 const productRouter = express.Router()
-const { getAllProducts } = require("../db/products")
+const { getAllProducts, createProduct } = require("../db/products")
 
 productRouter.get("/", async (req,res,next) => {
     try {
 
         const allproducts = await getAllProducts()
-        
+
         res.send(allproducts)
     } catch ({name, message}) {
         next({name, message})
     }
 })
+
+productRouter.post("/", async (req, res, next)=>{
+    const {name, price, description} = req.body
+    try {
+        const post = await createProduct ({name, price, description})
+
+        if (post){
+            res.send(post)
+        } else {
+            next({
+                name: "CreatePostError",
+                message: "Creating a post error has occurred"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}) 
+productRouter.post("/:post", async (req, res, next)=>{
+    const {name, price, description} = req.body
+    try {
+        const post = await createProduct ({name, price, description})
+
+        if (post){
+            res.send(post)
+        } else {
+            next({
+                name: "CreatePostError",
+                message: "Creating a post error has occurred"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}) 
 
 
 module.exports = productRouter
