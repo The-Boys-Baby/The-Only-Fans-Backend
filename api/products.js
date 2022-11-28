23a4d773b7c85cd100227ebdfacde307698cedc1
Dale Haase <dalehaase09@gmail.com>
2022-11-28 17:28:53 -0500

    delete product

--- api/products.js
+++ api/products.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const productRouter = express.Router()
-const { getAllProducts, createProduct, updateProduct } = require("../db/products")
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../db/products")
 
 productRouter.get("/", async (req,res,next) => {
     try {
@@ -36,15 +36,12 @@ productRouter.post("/:post", async (req, res, next)=>{
     const fields = {}
     if(req.body.name){
         fields.name = req.body.name
-
     }
     if(req.body.price){
         fields.price = req.body.price
-
     }
     if(req.body.description){
         fields.description = req.body.description
-
     }
 
     try {
@@ -62,6 +59,26 @@ productRouter.post("/:post", async (req, res, next)=>{
         console.log(error)
     }
 }) 
+
+productRouter.post("/:post/inactive", async (req, res, next) => {
+ const id = req.params.post
+
+ try {
+    const post = await deleteProduct(id)
+    console.log(id)
+    console.log(post)
+    if(post == true){
+        res.send(post)
+    }else{
+        next({
+            name:"inActiveError",
+            message:"This post is not in the system or is already inactive"
+        })
+    }
+ } catch (error) {
+    console.log(error)
+ }
+})
 
 
 module.exports = productRouter
