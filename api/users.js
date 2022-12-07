101e6e6827944137c02f14404459e531fca2c2b1
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2022-12-06 23:51:02 -0500

    admin

--- api/users.js
+++ api/users.js
@@ -1,55 +1,54 @@
-const express = require("express")
-const userRouter = express.Router()
+const express = require("express");
+const userRouter = express.Router();
 
-
-const { getUserByUsername, createUser} = require("../db/users")
-const {createOrder} = require("../db/order")
-const bcrypt = require("bcrypt")
+const { getUserByUsername, createUser } = require("../db/users");
+const { createOrder } = require("../db/order");
+const bcrypt = require("bcrypt");
 //importing jwt
-const jwt = require("jsonwebtoken")
-const { application } = require("express")
-const router = require(".")
+const jwt = require("jsonwebtoken");
+const { application } = require("express");
+const router = require(".");
 
 userRouter.post("/register", async (req, res, next) => {
-    
-    const {username, password, firstname, lastname, email} = req.body
-    console.log(username)
-    try {
-        const checkuser = await getUserByUsername(username)
+  const { username, password, firstname, lastname, email } = req.body;
+  console.log(username);
+  try {
+    const checkuser = await getUserByUsername(username);
 
-        if (checkuser) {
-            res.send({
-                status: "Unsuccessful",
-                name: "userExists",
-                message: "Username already exists, please input another username"
-            });
-        }
-        const user = await createUser({
-            username,
-            password,
-            firstname,
-            lastname,
-            email
-        });
-
-        const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET, {expiresIn: "1w"})
-        // console.log(token)
-        console.log(user.id)
-        await createOrder(user.id)
-        res.send({
-            user,
-            status: "Successful",
-            message: `Successfully created Username and password thank you for signing up ${username}`,
-            token
-        });
-
-    } catch (error) {
-        console.log(error)
-        next()
+    if (checkuser) {
+      res.send({
+        status: "Unsuccessful",
+        name: "userExists",
+        message: "Username already exists, please input another username",
+      });
     }
+    const user = await createUser({
+      username,
+      password,
+      firstname,
+      lastname,
+      email,
+    });
 
-
-})
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1w" }
+    );
+    // console.log(token)
+    // console.log(user.id);
+    await createOrder(user.id);
+    res.send({
+      user,
+      status: "Successful",
+      message: `Successfully created Username and password thank you for signing up ${username}`,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
 
 // // calling an async function after the create user function to hash and encrypt the password
 // async function hashedPassword (password){
@@ -57,7 +56,7 @@ userRouter.post("/register", async (req, res, next) => {
 //         // assigning saltValue to an await function to then generate the salt value
 //         const saltValue = await bcrypt.genSalt(8)
 //         console.log(`I am the salt value: ${saltValue}`)
-//         // now we are hashing our salt value by passing password and saltValue as a promise 
+//         // now we are hashing our salt value by passing password and saltValue as a promise
 //         const hashedValue = await bcrypt.hash(password, saltValue)
 //         console.log(`I am the hashed value: `, hashedValue)
 //     } catch (error) {
@@ -68,36 +67,70 @@ userRouter.post("/register", async (req, res, next) => {
 // hashedPassword('password');
 
 // using bcrpyt.compare we will tell if two values are one and the same
-userRouter.post("/login", async (req,res, next) => {
-    const {username,password} = req.body 
-   
-    if (!username, !password){
-        res.send({  name: "MissingCredentials",
-                message: "Please put in username and password"})
-    }
+userRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
 
-    try{
-    const user = await getUserByUsername(username)
+  if (!username || !password) {
+    res.send({
+      name: "MissingCredentials",
+      message: "Please put in username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
     // console.log(user)
-    const hashedpassword = user.password
+    const hashedpassword = user.password;
     // console.log(hashedpassword)
-    const validity = await bcrypt.compare(password, hashedpassword)
+    const validity = await bcrypt.compare(password, hashedpassword);
     // console.log(validity)
-    if(user && validity){
-        // console.log("did that")
-        const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET, {expiresIn: "1w"})
-        // console.log("token")
-        res.send({message: `thank you for logging in ${username}`, token: token})
-
-    }else{
-        res.send({name: "INVALIDUSERCREDENTIALS", 
-        message: "Invalid Username or password"})
+    if (user && validity) {
+      // console.log("did that")
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1w" }
+      );
+      // console.log("token")
+      delete user.password;
+      res.send({
+        message: `thank you for logging in ${username}`,
+        token,
+        user,
+      });
+    } else {
+      res.send({
+        name: "INVALIDUSERCREDENTIALS",
+        message: "Invalid Username or password",
+      });
     }
-    }catch(error){
-        // console.log(error)
-        next(error)
+  } catch (error) {
+    // console.log(error)
+    next(error);
+  }
+});
+userRouter.post("/me", async (req, res, next) => {
+  const token = req.body.token;
+  try {
+    if (!token) {
+      res.send({ message: "Eat it bitch" });
+    } else {
+      const verifiedInfo = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await getUserByUsername(verifiedInfo.username);
+      delete user.password;
+      delete user.firstname;
+      delete user.lastname;
+      // console.log(user);
+      res.send({
+        name: "SuccessWithToken",
+        message: "Sucessfully Logged In",
+        user: user,
+      });
     }
-})
+  } catch (error) {
+    // console.log(error)
+    next(error);
+  }
+});
 
-
-module.exports = userRouter
+module.exports = userRouter;
